0c89eccea0e48eab1bc87de0556e17e50f313774
andreasakarlsson <andreas.a.karlsson@gmail.com>
2018-02-26 12:15:42 +0100

    Minor renaming and small fixes

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,11 @@
 .onLoad <- function (lib, pkg) {
-    if(.Platform$OS.type == "unix") 
+    if(.Platform$OS.type == "unix")
         microsimulation.init(pkg)
 }
 
 .onUnload <- function (libpath) {
     pkg <- utils::packageName()
-    if(.Platform$OS.type == "unix") 
+    if(.Platform$OS.type == "unix")
         microsimulation.exit(pkg)
     library.dynam.unload(pkg, libpath)
 }
